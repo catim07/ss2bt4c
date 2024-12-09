81125adfcff84ebab2d07aa525a710bfa4bea30c
catim07 <cuongdfle@gmail.com>
2024-12-09 22:12:41 +0700

    Add files via upload

---
+++ ss2bt4.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int dodai = 5;
+    int  chuvi = 4 * dodai;
+    int dientich = dodai * dodai;
+    printf("Canh hinh vuong: %d\n", dodai);
+    printf("Chu vi hinh vuong: %d\n", chuvi);
+    printf("Dien tich hinh vuong: %d\n", dientich);
+
+    return 0;
+}
+
